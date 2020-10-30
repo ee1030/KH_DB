38ae872e375781ff09cdc8e39a01e239f039b3ab
ee1030 <sksguswoek1@gmail.com>
2020-10-30 12:06:02 +0900

    update : 02_함수
    날짜 처리 함수 실습 추가

--- 02_함수.sql
+++ 02_함수.sql
@@ -271,4 +271,28 @@ SELECT EMP_NAME,
     EXTRACT(DAY FROM HIRE_DATE) 입사일
 FROM EMPLOYEE;
 
+-- 1. EMPLOYEE 테이블에서 사원명, 입사일-오늘, 오늘-입사일 조회
+-- 단, 입사일-오늘의 별칭은 "근무일수1", 
+-- 오늘-입사일의 별칭은 "근무일수2"로 하고
+-- 모두 정수(내림)처리, 양수가 되도록 처리
+SELECT EMP_NAME,
+    ABS(FLOOR(HIRE_DATE-SYSDATE)) 근무일수1,
+    ABS(FLOOR(SYSDATE-HIRE_DATE)) 근무일수2
+FROM
+    EMPLOYEE;
 
+-- 2. EMPLOYEE 테이블에서 사번이 홀수인 직원들의 정보 모두 조회
+SELECT *
+FROM EMPLOYEE
+WHERE MOD (EMP_ID, 2) = 1;
+
+-- 3. EMPLOYEE 테이블에서 근무 년수가 20년 이상인 직원 정보 조회
+SELECT *
+FROM EMPLOYEE
+WHERE CEIL(MONTHS_BETWEEN(SYSDATE, HIRE_DATE)/12) >= 20;
+
+-- 4. EMPLOYEE 테이블에서 사원명, 입사일, 입사한 월의 근무일수를 조회
+SELECT EMP_NAME, HIRE_DATE,
+    EXTRACT(DAY FROM LAST_DAY(HIRE_DATE)) - EXTRACT(DAY FROM HIRE_DATE)
+FROM
+    EMPLOYEE;
